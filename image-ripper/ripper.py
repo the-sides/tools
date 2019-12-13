8f84a3a27ce71d4437e705bc667ef292c45fd849
the-sides <sides798@gmail.com>
2019-12-13 12:59:51 -0500

    combine pic and url

--- image-ripper/ripper.py
+++ image-ripper/ripper.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import csv
 
 try:
     os.mkdir('pics')
@@ -8,16 +9,20 @@ except OSError:
 else:
     print ("Successfully created the directory %s " % 'pics')
 
-urls = []
+urls = {}
 
-with open('urls.txt', 'r') as fin:
-  line = fin.readline().replace('\n', '') # Ignore newline character
-  while line:
-    urls.append(line)
-    line = fin.readline().replace('\n', '') # Ignore newline character
+with open('urls.csv', 'r') as fin:
+  reader = csv.DictReader(fin)
+  # print(reader)
+  for entry in reader: 
+    if entry['PIC'] and entry['Images'] and entry['Images'][0] != '':
+      print(entry['PIC'], entry['Images'].replace('\n', '').replace(' ', '').split(','))
+      urls[entry['PIC']] = entry['Images'].replace('\n', '').replace(' ', '').split(',')
 
-for i, url in enumerate(urls):
-  filename = url.split('/')[-1]
-  print(f'Downloading: {filename}')
-  r = requests.get(url, allow_redirects=True)
-  open(f'pics/{filename}', 'wb').write(r.content)
+
+for PIC in dict.keys(urls):
+  for url in urls[PIC]:
+    filename = f"{PIC}-{url.split('/')[-1]}"
+    print(f'Downloading: {PIC}:{filename} from {url}')
+    r = requests.get(url, allow_redirects=True)
+    open(f'pics/{filename}', 'wb').write(r.content)
